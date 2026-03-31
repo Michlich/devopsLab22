7009a43188c1b3528ef763366d71ad0c672e9d14
vboxuser <vboxuser@debOS.myguest.virtualbox.org>
2026-03-31 11:21:32 +0300

    14 commit

--- cicd/package.sh
+++ cicd/package.sh
@@ -3,8 +3,6 @@ set -e
 VERSION="1.0"
 PACKAGE_NAME="programm"
 echo "Creating .deb package..."
-mkdir -p build/$PACKAGE_NAME-$VERSION
-cp -r DEBIAN usr build/$PACKAGE_NAME-$VERSION/
-dpkg-deb --build build/$PACKAGE_NAME-$VERSION
-mv build/$PACKAGE_NAME-$VERSION.deb .
+mkdir -p build/
+dpkg-buildpackage -us -uc
 echo "Package $PACKAGE_NAME-$VERSION.deb created successfully"
